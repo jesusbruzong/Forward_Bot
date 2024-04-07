f91afd4c9bb92bc4a0f8225207be7252a1726846
jesusbruzong <47755683+jesusbruzong@users.noreply.github.com>
2024-04-07 23:25:18 +0200

    Update config.py

--- forward-bot-main/config.py
+++ forward-bot-main/config.py
@@ -1,3 +1,3 @@
-telegram_token = "5751454931:AAHZOgjIApv84n1haoOSKcvgyOEUXcDS_VA"
-chat_id_grupo_personal= 657490996
-chat_id_grupo_forward= -1001718547054
+telegram_token = ""
+chat_id_grupo_personal= 
+chat_id_grupo_forward= 
